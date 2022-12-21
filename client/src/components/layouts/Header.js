60145b576bd942fd0f6af400c7933bc2df6bcf73
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2022-12-21 09:18:22 +0100

    build

---
+++ client/src/components/layouts/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Footer } from './Footer'
+export const Header = () => {
+  return (
+    <div>
+        <img src='../../../public/quaTrello_logo.png'/>
+        <Footer/>
+    </div>
+  )
+}
